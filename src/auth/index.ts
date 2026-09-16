8a3d843c92223879ac912050f389fdd133aed66b
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-09-16 15:28:14 +0100

    chore: commit missing auth barrel file

---
+++ src/auth/index.ts
@@ -0,0 +1,4 @@
+export * from './presentation/auth.controller';
+export * from './presentation/middleware/session.middleware';
+export * from './domain/account.schema';
+export * from './domain/session.schema';
