ea1a43919847eae886f5e07c0e5562bbf4a523d5
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-09-16 10:37:41 +0100

    feat: add shared domain error classes

---
+++ src/shared/errors.ts
@@ -0,0 +1,20 @@
+export class NotFoundError extends Error {
+  constructor(resource: string, id: string) {
+    super(`${resource} with id "${id}" was not found.`);
+    this.name = 'NotFoundError';
+  }
+}
+
+export class ConflictError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ConflictError';
+  }
+}
+
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ValidationError';
+  }
+}
